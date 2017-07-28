46caeae72201b016667c54e0c2c5298feb5a215e
Fernando Pillo <fernando.pillo@ifood.com.br>
2017-07-28 08:11:02 -0300

    added save movie test cases

---
+++ src/test/java/com/elo7/search/usecases/SaveMovieTest.java
@@ -0,0 +1,55 @@
+package com.elo7.search.usecases;
+
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.validation.Validation;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.elo7.search.domains.Movie;
+import com.elo7.search.gateways.MovieGateway;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SaveMovieTest {
+
+    private MovieGateway movieGateway;
+
+    private ValidateDomain validateDomain;
+
+    private SaveMovie saveMovie;
+
+    @Before
+    public void setUp() {
+        movieGateway = Mockito.mock(MovieGateway.class);
+        validateDomain = new ValidateDomain(Validation.buildDefaultValidatorFactory().getValidator());
+
+        saveMovie = new SaveMovie(movieGateway, validateDomain);
+    }
+
+    @Test
+    public void test_save_valid_movie() {
+        saveMovie.save(movie("Alien", new HashSet<>(Arrays.asList( "Horror", "Sci-Fi")), 8.5f));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void test_save_invalid_movie() {
+        saveMovie.save(movie("Predator", new HashSet<>(Arrays.asList( "Action", "Sci-Fi", "Thriller")), null));
+    }
+
+    private Movie movie(final String name, final Set<String> genres, final Float grade) {
+        final Movie movie = new Movie();
+        movie.setTitle(name);
+        movie.setGenres(genres);
+        movie.setGrade(grade);
+
+        return movie;
+    }
+
+}
